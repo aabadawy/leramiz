c6acd33293624702785a41710e443b314dc91564
ahmed-Badawy-fciah <ahmedbadawy.fcia@gmail.com>
2020-05-11 00:53:42 +0200

    edit footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -5,18 +5,7 @@
 <footer class="footer-section set-bg" data-setbg="{{asset('css/img/footer-bg.jpg')}}">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-3 col-md-6 footer-widget">
-				<img src="img/logo.png" alt="">
-				<p>Lorem ipsum dolo sit azmet, consecter dipise consult  elit. Maecenas mamus antesme non anean a dolor sample tempor nuncest erat.</p>
-				<div class="social">
-					<a href="#"><i class="fa fa-facebook"></i></a>
-					<a href="#"><i class="fa fa-twitter"></i></a>
-					<a href="#"><i class="fa fa-instagram"></i></a>
-					<a href="#"><i class="fa fa-pinterest"></i></a>
-					<a href="#"><i class="fa fa-linkedin"></i></a>
-				</div>
-			</div>
-			<div class="col-lg-3 col-md-6 footer-widget">
+			<div class="col-lg-4 col-md-6 footer-widget">
 				<div class="contact-widget">
 					<h5 class="fw-title">CONTACT US</h5>
 					<p><i class="fa fa-map-marker"></i>3711-2880 Nulla St, Mankato, Mississippi </p>
@@ -25,7 +14,7 @@
 					<p><i class="fa fa-clock-o"></i>Mon - Sat, 08 AM - 06 PM</p>
 				</div>
 			</div>
-			<div class="col-lg-3 col-md-6 footer-widget">
+			<div class="col-lg-4 col-md-6 footer-widget">
 				<div class="double-menu-widget">
 					<h5 class="fw-title">POPULAR PLACES</h5>
 						<ul>
@@ -38,7 +27,7 @@
 						</ul>
 				</div>
 			</div>
-			<div class="col-lg-3 col-md-6  footer-widget">
+			<div class="col-lg-4 col-md-6  footer-widget">
 				<div class="newslatter-widget">
 					<h5 class="fw-title">NEWSLETTER</h5>
 					<p>Subscribe your email to get the latest news and new offer also discount</p>
@@ -52,12 +41,10 @@
 		<div class="footer-bottom">
 			<div class="footer-nav">
 				<ul>
-					<li><a href="">Home</a></li>
-					<li><a href="">Featured Listing</a></li>
-					<li><a href="">About us</a></li>
-					<li><a href="">Pages</a></li>
-					<li><a href="">Blog</a></li>
-					<li><a href="">Contact</a></li>
+					<li><a href="/">Home</a></li>
+					<li><a href="/property">All Properties</a></li>
+					<li><a href="#">About us</a></li>
+					<li><a href="#">Contact</a></li>
 				</ul>
 			</div>
 			<div class="copyright">
